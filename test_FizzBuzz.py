e4ea84c85d5505b84b50305509fd9e5c7df949f9
borngasp <56008554+borngasp@users.noreply.github.com>
2021-03-07 17:02:41 -0800

    Added test 2
    This test checks if when the number is divisible by three and not by five it returns Fizz

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -7,6 +7,11 @@ class FizzBuzzTestCases(unittest.TestCase):
     def test1(self):
         for x in range(1, 101):
             self.assertEqual(type(FizzBuzz.FizzBuzz(x)),str)
+    
+    def test2(self):
+        for x in range(1, 101):
+            if x%3 == 0 and x%5 != 0:
+                self.assertEqual(FizzBuzz.FizzBuzz(x),"Fizz")
 
 if __name__ == "__main__":
     unittest.main()
